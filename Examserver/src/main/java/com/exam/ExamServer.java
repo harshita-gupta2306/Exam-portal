4b09ae82e7ea4e40f22b20d83ade303f1711076c
harshita-gupta2306 <89689962+harshita-gupta2306@users.noreply.github.com>
2022-10-11 17:43:36 +0530

    Add files via upload

---
+++ Examserver/src/main/java/com/exam/ExamServer.java
@@ -0,0 +1,46 @@
+package com.exam;
+
+import com.exam.model.Role;
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class ExamServer implements CommandLineRunner {
+
+    @Autowired
+    private UserService userService;
+	public static void main(String[] args) {
+
+        SpringApplication.run(ExamServer.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Starting code");
+//        User user =new User();
+//        user.setFirstName("Harshita");
+//        user.setLastName("Gupta");
+//        user.setUsername("harsita2001");
+//        user.setPassword("");
+//        user.setEmail("harsita2001@gmail.com");
+//        user.setProfile("default.png");
+//        Role role1=new Role();
+//        role1.setRoleId(44L);
+//        role1.setRoleName("Admin");
+//        Set<UserRole> userRoleSet=new HashSet<>();
+//        UserRole userRole=new UserRole();
+//        userRole.setRole(role1);
+//        userRole.setUser(user);
+//        userRoleSet.add(userRole);
+//        User user1=this.userService.createUser(user,userRoleSet);
+//        System.out.println(user1.getUsername());
+    }
+}
